3a714f08347e65e42be8a7df28d2f757365e0a47
oftheseven <francesco.del7@outlook.it>
2025-12-03 16:00:04 +0100

    update
    aggiunta funzione in cui avviene il movimento dell'utente (al posto di avere tutto nell'update)

--- VR_gruppo6/Assets/Scripts/PlayerController.cs
+++ VR_gruppo6/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Vector3 spawnPoint = Vector3.zero;
-    [SerializeField] private float speed = 5f;
-    [SerializeField] private float runSpeed = 10f;
-    [SerializeField] private float rotationSpeed = 3f;
-    [SerializeField] private float maxLookAngle = 80f; // Limite per guardare su/giù
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _runSpeed = 10f;
+    [SerializeField] private float _rotationSpeed = 3f;
+    [SerializeField] private float _maxLookAngle = 80f; // Limite per guardare su/giù
 
     private float verticalRotation = 0f;
 
@@ -17,7 +17,12 @@ public class PlayerController : MonoBehaviour
     }
 
     // da modificare, non possiamo lasciare tutto nell'update, questo era giusto per provare al volo
-    void Update()
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         Vector2 moveInput = Vector2.zero;
         Vector2 lookInput = Vector2.zero;
@@ -60,16 +65,16 @@ public class PlayerController : MonoBehaviour
         Vector3 forward = new Vector3(transform.forward.x, 0, transform.forward.z).normalized;
         Vector3 right = new Vector3(transform.right.x, 0, transform.right.z).normalized;
         
-        Vector3 move = (forward * moveInput.y + right * moveInput.x) * speed * Time.deltaTime;
+        Vector3 move = (forward * moveInput.y + right * moveInput.x) * _moveSpeed * Time.deltaTime;
         this.transform.position += move;
 
         // Rotazione orizzontale (Y axis)
-        Vector3 horizontalRotation = new Vector3(0, lookInput.x, 0) * rotationSpeed * Time.deltaTime;
+        Vector3 horizontalRotation = new Vector3(0, lookInput.x, 0) * _rotationSpeed * Time.deltaTime;
         this.transform.Rotate(horizontalRotation, Space.World);
 
         // Rotazione verticale (X axis) con limiti
-        verticalRotation -= lookInput.y * rotationSpeed * Time.deltaTime;
-        verticalRotation = Mathf.Clamp(verticalRotation, -maxLookAngle, maxLookAngle);
+        verticalRotation -= lookInput.y * _rotationSpeed * Time.deltaTime;
+        verticalRotation = Mathf.Clamp(verticalRotation, -_maxLookAngle, _maxLookAngle);
         this.transform.localEulerAngles = new Vector3(verticalRotation, this.transform.localEulerAngles.y, 0);
     }
 }
