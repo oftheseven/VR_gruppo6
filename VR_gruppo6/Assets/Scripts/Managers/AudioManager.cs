8bc42271a9969aa778811d249764a5faf30c041d
oftheseven <francesco.del7@outlook.it>
2025-12-14 11:50:16 +0100

    aggiunto un po' di funzioni di base all'audio manager

--- VR_gruppo6/Assets/Scripts/Managers/AudioManager.cs
+++ VR_gruppo6/Assets/Scripts/Managers/AudioManager.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioSource _audioSourceLoop;
+    // singleton
+    private static AudioManager _audioManager;
+    public static AudioManager instance => _audioManager;
+    public Button audioButton;
+
+    public void Awake()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _audioManager = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAudio(AudioClip clip)
     {
-        
+        _audioSource.PlayOneShot(clip);
+    }
+
+    public void PlayAudioLoop(AudioClip clip)
+    {
+        _audioSourceLoop.clip = clip;
+        _audioSourceLoop.Play();
+    }
+
+    public void StopAudio()
+    {
+        _audioSource.Stop();
+        _audioSourceLoop.Stop();
     }
 }
