116eea97710c9821e8e417a49d133ac96681f0bf
Francesco Del Sette <francesco.delsette@students.uniroma2.eu>
2026-01-12 18:59:52 +0100

    UI_ViewCameraPanel

---
+++ VR_gruppo6/Assets/Scripts/UI/UI_ViewCameraPanel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class UI_ViewCameraPanel : MonoBehaviour
+{
+    // singleton
+    private static UI_ViewCameraPanel _instance;
+    public static UI_ViewCameraPanel instance => _instance;
+}
